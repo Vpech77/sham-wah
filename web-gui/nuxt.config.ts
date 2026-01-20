0b0009668a9aeb1480f294d20c0e52cd5ba8e2c5
vpech77 <vanessa.pech77@gmail.com>
2026-01-20 18:44:30 +0100

    feat: update nuxt configuration to include app head settings and favicon

--- web-gui/nuxt.config.ts
+++ web-gui/nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: "Sham-Wah App",
+      meta: [
+        { name: "description", content: "Description de mon application" },
+      ],
+    },
+  },
+
   modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
 });

a20cf40b4c8f19b6d368956dcca94e884bf4c465
vpech77 <vanessa.pech77@gmail.com>
2026-01-20 12:09:14 +0100

    chore: add tailwind and pinia

--- web-gui/app/app.vue
+++ web-gui/app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold text-indigo-600">Tailwind OK</h1>
   </div>
 </template>
